83377a5f5a20a7078ec5ec7db3d82907bbf2a798
deokarmeldeki <dekisiansahan64@gmail.com>
2020-11-17 03:53:01 +0700

    edit form detail produk

--- system/resources/views/produk/show.blade.php
+++ system/resources/views/produk/show.blade.php
@@ -6,16 +6,18 @@
 		<div class="row">
 			<div class="col-md-12">
 				<div class="card mt-4">
-					<div class="card-header">
+					<div class="card-header text-bold">
 						Detail Data Produk
 					</div>
 					<div class="card-body">
-						<h4>{{$produk->nama}}</h4><hr>
-						<p>Berat {{number_format($produk->harga)}} Kg |
+
+							<h4>{{$produk->nama}}</h4><hr>
+						<table class="table table-bordered">
+							<th>Berat {{number_format($produk->harga)}} Kg |
 							Rp : {{$produk->berat}}  |
-							Stok : {{$produk->stok}}
-						</p>
-						{!! nl2br($produk->deskripsi) !!}
+							Stok : {{$produk->stok}} unit</th>
+						</table><br>
+							<p>{!! nl2br($produk->deskripsi) !!}</p>
 					</div>
 				</div>
 			</div>
